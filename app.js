10ba9ff11baef165427aca5f110b78ebe30291b5
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-29 16:12:34 +0200

    install and set up mongoose for the application

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+
+//Mongoose structure
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model('User', userSchema);
 
 //GET requests for homepage, login and register
 app.get('/', function(req,res){
