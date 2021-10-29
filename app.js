b156c0fa0d26fd553c0a9f49314d953ae84e3de4
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-29 16:07:12 +0200

    add get requests for home, login and register pages

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 
-
-
+//GET requests for homepage, login and register
+app.get('/', function(req,res){
+  res.render('home');
+});
+app.get('/login', function(req,res){
+  res.render('login');
+});
+app.get('/register', function(req,res){
+  res.render('register');
+});
 const port = 3000
 app.listen(port, () => console.log("app started on port " + port))
