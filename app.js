f881ad6c719960b006d974ebb7b3f38716b4d5dc
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-11-01 15:31:20 +0100

    change encryption to a more secure system hashing with md5

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
 const app = express();
+const md5 = require('md5');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -17,8 +16,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]} );
 
 const User = new mongoose.model('User', userSchema);
 
@@ -38,7 +35,7 @@ app.get('/register', function(req,res){
 app.post('/register', function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password) //uses MD5 to turn the password into an irreversible hash
   });
   newUser.save(function(err){
     if(err){
@@ -50,7 +47,7 @@ app.post('/register', function(req,res){
 });
 app.post('/login', function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}, function(err,foundUser){
     if(err){
       console.log(err)
