67cde844f7ef717a36130967c517d9dab5f41100
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-11-03 09:19:37 +0100

    update login and register system using passport

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Secrets
  Single Page Application that allows to add anonymous statements. 
+
+## Tools
+ - Node.js
+ - Express.js
+ - Passport.js
+ - express sessions
+ - EJS templating
+
+## How it works?
+
+The Secrets application was created in order to understand and go deeper into the complexities of web security, authorization and authentication. It works with passport.js, although previous branches and commits have used MD5 hashing, bcrypt and mongoose-encryption.
+
+It also works with cookies and sessions, which are the most secure cookies for logged users.
+
+The app allows users to create a secure user and add anonymous secrets to the application, so that people can read them without knowing who wrote them. It is a very interesting (and funny) way to practice this security systems, and there are many sites out there that are used for the same purpose.
+
+## Deployment 
+
+ - Fork it on your github account, and clone it locally.
+ - From your Command Line Interface, ``cd`` into the folder and run the first package.json script: ``npm start`` . It will launch parcel and bundle up the code.
+ - Once budled up, it will serve the file on port 1234. Run it from the browser, and the site will be there
--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+require('dotenv').config()
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+//express session
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 //Mongoose structure
@@ -16,8 +32,16 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+//Passport-Local Mongoose will add the hashed password and the salt value.
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model('User', userSchema);
 
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //GET requests for homepage, login and register
 app.get('/', function(req,res){
@@ -29,13 +53,48 @@ app.get('/login', function(req,res){
 app.get('/register', function(req,res){
   res.render('register');
 });
+app.get('/secrets', function(req,res){
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  } else {
+    res.redirect('/login');
+  }
+})
+
+app.get('/logout', function(req,res){
+  req.logout();
+  res.redirect('/')
+})
 
 //POST request for register and login
 app.post('/register', function(req,res){
-
+  //method that comes from passport local mongoose. creates a new user on User model. 
+ User.register({username: req.body.username}, req.body.password, function(err, user){  
+   if (err){
+     console.log(err);
+     res.redirect('/register');
+   } else {
+     passport.authenticate('local')(req, res, function(){ //function that redirects to secrets page
+       res.redirect('/secrets');
+     })
+   }
+ })
 });
 app.post('/login', function(req,res){
-  
+ const user = new User({
+   username: req.body.username,
+   password: req.body.password
+ });
+ //method coming from passport local mongoose. called on the req, it automatically checks if user shows in the DB
+ req.login(user, function(err){
+   if(err){
+     console.log(err);
+   }else{
+     passport.authenticate('local')(req, res, function(){
+       res.redirect('/secrets');
+     })
+   }
+ })
 });
 
 
