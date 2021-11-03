3755e1489b60cc08089a75112434bf385b5f8771
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-11-03 07:50:23 +0100

    install passport. remove all POST content we had before

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const app = express();
-const bcrypt = require("bcrypt");
-const saltRounds = 10; // how many times we run salt rounds and hash passwords to make slower the hashing generator processs
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -34,43 +32,10 @@ app.get('/register', function(req,res){
 
 //POST request for register and login
 app.post('/register', function(req,res){
-  //everything goes inside the next bcrypt hash function. 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){ // uses bcrypt to hash password with the defined number of saltrounds
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }else{
-        res.render('secrets');
-      }
-    });
-  });
 
 });
 app.post('/login', function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username}, function(err,foundUser){
-    if(err){
-      console.log(err)
-    } else {
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result){ // uses bcrypt to compare password with the hashed password
-           if(result === true){
-             console.log("user accessed")
-            res.render("secrets");
-           } else {
-             console.log("Incorrect password")
-           }
-        });
-      } else{
-        console.log("no such user")
-      }
-    }
-  });
+  
 });
 
 
